622fe7c8df4d0df91c8e52e497513a286ba1fffb
saifrock5429-droid <saifrock5429@gmail.com>
2026-05-24 00:27:14 +0530

    Update CategoryButtons.jsx

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CategoryButtons = ({ activeCategory, setActiveCategory }) => {
-  const cats = ["Ladies Watch", "Mens Watch", "Ladies Sunglasses", "Mens Sunglasses", "Belts", "All Bags", "Shoes", "All Categories"];
+  const cats = ["Ladies Watch", "Mens Watch", "Ladies Sunglasses", "Mens Sunglasses", "Belts & Wallets", "All Bags", "Shoes", "All Categories"];
 
   return (
     <div className="max-w-[95%] md:max-w-[80%] mx-auto my-8">
